5d3e7354149fa0ca7bb600472976308e6f38a6d2
Gagnon <gagnon_alex@videotron.ca>
2017-07-24 12:28:11 -0400

    filtre rii

--- rif_filter.m
+++ rif_filter.m
@@ -14,8 +14,8 @@ result = filter(allo, s1_down);
 % wvtool(result);
 
 %% Banc de filtre 
-
-%determiner les 6 raies spectrales très franches
+fe = 8000;
+% determiner les 6 raies spectrales très franches
 spectre = abs(fft(s1_down, fe_down));
 plot(spectre);
 
@@ -35,28 +35,48 @@ for k = 1 : min([6, length(sortedValues)])
     k, sortedValues(k), originalLocations(k));
     %matrix(row, column)
     raies_spectrales(k,1) =sortedValues(k) ; % premiere colonne = amplitude
-    raies_spectrales(k,2) =originalLocations(k) ; % deuxieme colonne = freqs
+    raies_spectrales(k,2) =originalLocations(k) ; % deuxieme colonne = freqs normalise sur 1(pi)
     
 end
 
 text(raies_spectrales(:,2)+.02,raies_spectrales(:,1),num2str((1:numel(raies_spectrales(:,1)))'))
 
+%normalise les frequences
+ raies_spectrales(:,2) = raies_spectrales(:,2)./fe*2
 %% design des filtre passe-bande
-delta = 1/16;
-[a1 b1] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta,'bandpass');
-plot(butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta,'bandpass'));
+delta = 1/160;
 
-[a2 b2] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass');
-plot(
+hold on 
+[b1 a1] = butter(2,[raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta],'bandpass');
+fvtool(b1,a1);   
+title(['butter laissant passer ', num2str(raies_spectrales(1,2)*fe/2),' Hz']);
 
-[a3 b3] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass');
-plot(
 
-[a4 b4] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass');
-plot(
+[b2 a2] = butter(2,[raies_spectrales(2,2)-delta,raies_spectrales(2,2)+delta],'bandpass');
+fvtool(b2,a2); 
+title(['butter laissant passer ', num2str(raies_spectrales(2,2)*fe/2),' Hz']);
 
-[a5 b5] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass');
-plot(
 
-[a6 b6] = butter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass');
-plot(nutter(2,raies_spectrales(1,2)-delta,raies_spectrales(1,2)+delta),'bandpass'););
+[b3 a3]= butter(2,[raies_spectrales(3,2)-delta,raies_spectrales(3,2)+delta],'bandpass');
+fvtool(b3,a3); 
+title(['butter laissant passer ', num2str(raies_spectrales(3,2)*fe/2),' Hz']);
+
+
+[b4 a4] = butter(2,[raies_spectrales(4,2)-delta,raies_spectrales(4,2)+delta],'bandpass');
+fvtool(b4,a4); 
+title(['butter laissant passer ', num2str(raies_spectrales(4,2)*fe/2),' Hz']);
+
+
+[b5 a5] = butter(2,[raies_spectrales(5,2)-delta,raies_spectrales(5,2)+delta],'bandpass');
+fvtool(b5,a5); 
+title(['butter laissant passer ', num2str(raies_spectrales(5,2)*fe/2),' Hz']);
+
+
+[b6 a6] = butter(2,[raies_spectrales(6,2)-delta,raies_spectrales(6,2)+delta],'bandpass');
+fvtool(b6,a6); 
+title(['butter laissant passer ', num2str(raies_spectrales(6,2)*fe/2),' Hz']);
+
+
+s1 = filter(b1,a1,result);
+plot(abs(fft(s1, fe_down)));
+hold off
